acbfdb49c153f68e92673ba652f3626f1fce0f9c
Pawegit config --global user.name Pawel <szczerbicki.pawel@gmail.com>
2013-06-02 19:46:34 +0200

    dodatkowe zmiany

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -4,6 +4,7 @@ import DAO.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,11 @@ public class ProductController {
         productDao.deleteProductById(id);
         model.addAttribute("products", productDao.getProductList());
         return "index";
+    }
+    @RequestMapping(value = "/show/${id}", method = RequestMethod.GET)
+    public String showProduct(@PathVariable String id, Model model) {
+        model.addAttribute("product", productDao.getProductById(id));
+        return "details";
     }
 
 }
